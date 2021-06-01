f0dcc07a9de5d0beadb98497191e466ce8c2a0b3
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-06-01 16:25:24 -0500

    Ruta-Laravel8

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+use App\Http\Controllers\ProfilesController;
+use App\Http\Controllers\ExploreController;
 
 
 Route::redirect('/', 'tweets');
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::view('/tweets', 'tweets.index')->name('home');
-    Route::get(
-        '/profiles/{user:username}/edit',
-        'ProfilesController@edit'
-    )->middleware('can:edit,user');
 
-    Route::get('/explore', 'ExploreController');
 
-    Route::get('/profiles/{user:username}', 'ProfilesController@show')->name(
+    Route::get('/profiles/{user:username}/edit', [ProfilesController::class, 'edit'])->middleware('can:edit,user');
+
+
+    Route::get('/explore', ExploreController::class);
+
+    Route::get('/profiles/{user:username}', [ProfilesController::class,'show'])->name(
         'profile'
     );
 });
